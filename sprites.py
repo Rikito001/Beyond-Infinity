e525c0d2ad979476f27ee34ec018786ea84b0124
Rikito001 <simi.kovacik@gmail.com>
2025-01-08 12:55:34 +0100

    Basic imports and class definitions
    Created basic classes and their constructors, added imports and basic links between classes

---
+++ sprites.py
@@ -0,0 +1,14 @@
+from settings import *
+
+
+class Sprite(pygame.sprite.Sprite):
+    def __init__(self, position, surface, groups):
+        super().__init__(groups)
+        self.image = surface
+        self.rect = self.image.get_frect(topleft=position)
+
+class CollisionSprites(pygame.sprite.Sprite):
+    def __init__(self, position, surface, groups):
+        super().__init__(groups)
+        self.image = surface
+        self.rect = self.image.get_frect(topleft=position)
